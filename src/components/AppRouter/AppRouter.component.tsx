677b12e528500dccca91105c96b66a499fbd0f60
pavel <pavel.sergutin@cronix.ms>
2018-10-26 12:56:46 +0300

    not found page

--- src/components/AppRouter/AppRouter.component.tsx
+++ src/components/AppRouter/AppRouter.component.tsx
@@ -4,6 +4,8 @@ import * as React from "react";
 import {Redirect, Route, Switch} from "react-router-dom";
 // pages
 import AppHeaderComponent from "../AppHeader/AppHeader.component";
+import AppAuthComponent from "../AppAuth/AppAuth.component";
+import NotFoundComponent from "../../pages/NotFound/NotFound.component";
 import DashboardComponent from "../../pages/Dashboard/Dashboard.component";
 import HomeComponent from "../../pages/Home/Home.component";
 
@@ -15,11 +17,13 @@ const AppRouterComponent: React.SFC<any> = (props?) => {
       <AppHeaderComponent />
       <Switch>
         <Redirect from="/" to="/dashboard" exact={true} />
+        <Route path="/auth" component={AppAuthComponent} />
         <Route path="/dashboard" component={DashboardComponent} />
         <Route path="/home" component={HomeComponent} />
         {/*<Route path="/login" component={SignInPage} />*/}
         {/*<PrivateRoute path="/" component={ProtectedContent} />*/}
-        <Route component={HomeComponent} />
+        <Route path="/not-found" component={NotFoundComponent} />
+        <Redirect to="/not-found" />
       </Switch>
     </div>
   );
